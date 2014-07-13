3e2683622412b2fa81001a1cc75c6ce024cafaee
ddrieck <ddrieck@gmail.com>
2014-07-12 21:38:44 -0700

    Finished basic install with dependencies.

---
+++ server.js
@@ -0,0 +1,22 @@
+//==========
+//BASE SETUP
+//==========
+
+//assign and call needed packages
+var express = require('express');
+var app = express();
+
+//Load up our route and model config files
+var database = require('./app/configs/database');
+var routes = require('./app/routes/routes');
+var Product = require('./app/models/product');
+
+//Connect to the database
+mongoose.connect(database.url);
+
+//Set our port. Heroku has 5000 as example so we'll do that
+var port = process.env.PORT || 5000;
+
+//================
+//START THE SERVER
+//================
