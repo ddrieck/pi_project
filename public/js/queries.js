5815ab9de94fcd91775aeb8f0370b7627df3efb5
ddrieck <ddrieck@gmail.com>
2014-07-14 01:08:17 -0700

    Fixed URL for heroku test.

--- public/js/queries.js
+++ public/js/queries.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var category = 'TRIM-A-TREE';
-	var catAPI = 'http://pi-project.herokuapp.com/api/category/parent/' + category;
+	var catAPI = 'http://pi-project.herokuapp.com/api/products/category/parent/' + category;
 	$.getJSON(catAPI, function(data){
 		console.log(data);
 	});
