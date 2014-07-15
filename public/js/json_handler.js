1cd2b9c36093295f2e7f57b42e4bd0e6fc42d6f4
ddrieck <ddrieck@gmail.com>
2014-07-15 10:20:18 -0700

    Added search bar actions and list population.

--- public/js/json_handler.js
+++ public/js/json_handler.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
-	var apiURL = 'http://pi-project.herokuapp.com/api/products/';
-	var searchURL = 'http://pi-project.herokuapp.com/api/products/search/';
+	var apiURL = 'http://localhost:5000/api/products/';
+	var searchURL = 'http://localhost:5000/api/products/search/';
 
 $('#search-form').submit(function(event){
 		event.preventDefault();
 		var keyword = $('.search-bar').val();
-		console.log("hello")
-		console.log(keyword);
+		$(".results").empty();
+		$("#search-section").css("margin-top", "2%");
 		if ($('.select-bar').val() === "products"){
 			$.getJSON(searchURL + keyword, function(data){
 				console.log(data);
-				});	
+				if (data.length === 0){
+					$('.results').append("<h1>No Results</h1>");
+				} else {
+					for (var i = 0; i < data.length; i++) {
+						$('.results').append("<p>" + data[i].title + "</br>By: " + data[i].brand + "</br>" + data[i].upc + "</p>");
+					};
+				};
+			});	
 		} else {
 			$.getJSON(searchURL + 'category/' + keyword, function(data){
-				console.log(data);
-				});	
+				if (data.length === 0){
+					$('.results').append("<h1>No Results</h1>");
+				} else {
+					for (var i = 0; i < data.length; i++) {
+						$('.results').append("<p>" + data[i].parentCategory + "</br>" + data[i].category + "</p>");
+					};
+				};
+			});	
 		};
 	});
 });
