de7cd4c619bfbf5401f9ffe617c2089996e08835
ddrieck <ddrieck@gmail.com>
2014-07-30 23:10:03 -0700

    Deleting unused files.

--- public/js/queries.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	var category = 'TRIM-A-TREE';
-	var catAPI = 'http://pi-project.herokuapp.com/api/products/category/parent/' + category;
-	$.getJSON(catAPI, function(data){
-		console.log(data);
-	});
-});
